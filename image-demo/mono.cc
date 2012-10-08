5710408b5274f5d081df2efc7477fc4efa38faad
navyxliu <navy.xliu@gmail.com>
2012-10-08 22:22:00 +0800

    port to opencl1.0

--- image-demo/mono.cc
+++ image-demo/mono.cc
@@ -5,7 +5,8 @@
 #include "cl_simple.h"
 #include "cl_util.h"
 #include "imgsupport.hpp"
-
+#define IMAGE_DIM_X 48 //in_img.getWidth()
+#define IMAGE_DIM_Y 48 // in_img.getHeight()
 int main()
 {
     cl_uchar * input;
@@ -16,7 +17,7 @@ int main()
     vina::PngImage in_img, out_img;
 
     in_img.loadFromFile("lena.png");
-    out_img.storeToFile("mono.png", in_img.getWidth(), in_img.getHeight(), 
+    out_img.storeToFile("mono.png", IMAGE_DIM_X, IMAGE_DIM_Y,
         in_img.getBitDepth(), in_img.getColorType());
 
     printf("loaded image width = %u, height = %u BPP = %d RowBytes = %d\n", 
@@ -24,7 +25,15 @@ int main()
         in_img.getBPP(), in_img.getRowBytes());
     
     input = in_img.getData(); 
-    data_size = in_img.getHeight() * in_img.getRowBytes();
+    //data_size = in_img.getHeight() * in_img.getRowBytes();
+    data_size = IMAGE_DIM_X * IMAGE_DIM_Y * 4; //3 * in_img.getHeight() * in_img.getRowBytes();
+
+    unsigned char * buffer_v4 = (unsigned char *)malloc(data_size);
+    for (int i = 0; i<IMAGE_DIM_X; ++i) for (int j=0; j<IMAGE_DIM_Y; ++j) {
+        buffer_v4[i * IMAGE_DIM_X * 4 + j * 4]  = input[i * in_img.getRowBytes() + j * 3];
+        buffer_v4[i * IMAGE_DIM_X * 4 + j * 4 + 1] = input[i * in_img.getRowBytes() + j * 3 + 1];
+        buffer_v4[i * IMAGE_DIM_X * 4 + j * 4 + 2] = input[i * in_img.getRowBytes() + j * 3 + 2];
+    }
 
 #if 1
     struct cl_simple_context ctx;
@@ -51,17 +60,24 @@ int main()
     }
 
     clSimpleSetOutputBuffer(&ctx, data_size);
-    in_buffer = clCreateBuffer(ctx.cl_ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, data_size, input, NULL);
+    in_buffer = clCreateBuffer(ctx.cl_ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, data_size, buffer_v4, NULL);
     clSetKernelArg(ctx.kernel, 1, sizeof(cl_mem), &in_buffer);
 
-    global_work_size[0] = in_img.getWidth();
-    global_work_size[1] = in_img.getHeight();
-    local_work_size[0] = 4;
-    local_work_size[1] = 4;
-    if ( clSimpleEnqueueNDRangeKernel(ctx.command_queue, ctx.kernel, 2, global_work_size, local_work_size) ) {
+    global_work_size[0] = IMAGE_DIM_X * IMAGE_DIM_Y;
+    //global_work_size[1] = 48
+    local_work_size[0] = 16 * 16;
+    //local_work_size[1] = 48;
+    if ( clSimpleEnqueueNDRangeKernel(ctx.command_queue, ctx.kernel, 1, global_work_size, local_work_size) ) {
         fprintf(stderr, "clEnqueueNDRangeKernel() suceeded.\n");
         clSimpleReadOutput(&ctx, output, data_size);
-        out_img.setData(output);
+
+        for (int i = 0; i<IMAGE_DIM_X; ++i) for (int j=0; j<IMAGE_DIM_Y; ++j) {
+            buffer_v4[i * in_img.getRowBytes() + j * 3]  = output[i * IMAGE_DIM_X * 4 + j * 4];
+            buffer_v4[i * in_img.getRowBytes() + j * 3 + 1] = output[i * IMAGE_DIM_X * 4 + j * 4 + 1];
+            buffer_v4[i * in_img.getRowBytes() + j * 3 + 2] = output[i * IMAGE_DIM_X * 4 + j * 4 + 2];
+        }
+
+        out_img.setData(buffer_v4);
         return EXIT_SUCCESS;
     }
     else {
--- image-demo/mono.cl
+++ image-demo/mono.cl
@@ -1,9 +1,9 @@
 
-__kernel void mono(__global uchar3 * out, __global const uchar3 * in)
+__kernel void mono(__global uchar4 * out, __global const uchar4 * in)
 {
     size_t id = get_global_id(0);
-    __const float3 gMonoMult = {0.299f, 0.587f, 0.114f};
+    __const float4 gMonoMult = {0.299f, 0.587f, 0.114f, .0f};
 
-    float3 pixel = (float3)(in[id].x, in[id].y, in[id].z);
-    out[id] = (uchar3)dot(pixel, gMonoMult);
+    float4 pixel = (float4)(in[id].x, in[id].y, in[id].z);
+    out[id] = (uchar4)dot(pixel, gMonoMult);
 }
